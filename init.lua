c216a6441ebd29daa815bea3eb809d88ed64f2f6
erbeeen <irvinkierpresto26@gmail.com>
2024-08-02 21:26:29 +0800

    Add kotlin to lsp

--- init.lua
+++ init.lua
@@ -555,6 +555,7 @@ require('lazy').setup({
         html = {},
         emmet_language_server = {},
         sqlls = {},
+        kotlin_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
