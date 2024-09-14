97410824316d2f15eaa16d4ea1d62973f6bf19c2
erbeeen <irvinkierpresto26@gmail.com>
2024-09-14 13:30:59 +0800

    Switch from pyright to pylsp

--- init.lua
+++ init.lua
@@ -552,7 +552,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         gopls = {},
-        pyright = {},
+        pylsp = {},
         cssls = {},
         html = {},
         emmet_language_server = {},
