076363384e80c11e4e77d75bd7bf6320acc55723
erbeeen <irvinkierpresto26@gmail.com>
2025-04-17 04:05:33 +0800

    Add scala lsp

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -178,4 +178,37 @@ return {
       -- Your settings go here
     },
   },
+
+  -- NOTE: Scala LSP
+  {
+    'scalameta/nvim-metals',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    ft = { 'scala', 'sbt', 'java' },
+    opts = function()
+      local metals_config = require('metals').bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+      end
+      metals_config.init_options.statusBarProvider = "off"
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      metals_config.settings = {
+        showImplicitArguments = true
+      }
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      -- require('lspconfig').metals.setup{}
+      local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = self.ft,
+        callback = function()
+          require('metals').initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
 }
